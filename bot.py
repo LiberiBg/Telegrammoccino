4b91a3d384e6450617a9e680e8f0cbbbe8b4d187
Matheo Nguyen <matheo.ngn@gmail.com>
2023-05-10 23:52:39 +0200

    qr code validé

--- bot.py
+++ bot.py
@@ -1,13 +1,15 @@
-import telegram, logging, functions, dotenv, os
-from telegram.ext import (
-        ApplicationBuilder,
-        ContextTypes,
-        CommandHandler,
-        filters,
-        MessageHandler 
-        )
-from telegram import Update, MessageEntity
+import dotenv
+import functions
+import logging
+import os
 
+from telegram.ext import (
+    ApplicationBuilder,
+    CommandHandler,
+    filters,
+    MessageHandler,
+    ConversationHandler, CallbackQueryHandler
+)
 
 #Configure a logs template
 logging.basicConfig(
@@ -21,23 +23,25 @@ dotenv.load_dotenv()
 # Récupérer la valeur de la variable d'environnement TOKEN
 TOKEN = os.environ.get('TOKEN')
 
-
-
+QRCODE_START, QRCODE_END = range(2)
 if __name__ == '__main__':
 
-    application = ApplicationBuilder().token(TOKEN).build()
+    application = ApplicationBuilder().token(TOKEN).concurrent_updates(True).build()
 
-    start_handler = CommandHandler('start', functions.start) 
-    echo_handler = MessageHandler(filters.TEXT & (~filters.COMMAND), functions.echo)
-    unknown_handler = MessageHandler(filters.COMMAND, functions.unknown)
+    application.add_handler(CommandHandler('start', functions.start))
+    application.add_handler(ConversationHandler(
+        entry_points=[CommandHandler("qrcode", functions.ask_qrcode_url)],
+        states={
+            QRCODE_START: [MessageHandler(filters.TEXT & ~filters.COMMAND, functions.get_qrcode)],
+            QRCODE_END: [MessageHandler(filters.TEXT & ~filters.COMMAND, functions.get_qrcode)],
+        },
+        fallbacks=[CommandHandler("cancel", functions.cancel)],
+        allow_reentry=True,
+        name='Conversation generate qrcode from url'
+    ))
+    application.add_handler(CallbackQueryHandler(functions.handle_callback_query))
+    #application.add_error_handler(functions.error)
+    #application.add_handler(MessageHandler(filters.COMMAND, functions.unknown))
 
-    application.add_handler(start_handler)
-    application.add_handler(echo_handler)
-    application.add_handler(unknown_handler)
-
+    print("Bot's polling...")
     application.run_polling()
-
-   
-
-
-   
--- functions.py
+++ functions.py
@@ -1,33 +1,37 @@
-import logging, pyqrcode
+import logging
+import os
+import pyqrcode
+
 from telegram import (
-        Update,
-        InlineKeyboardButton,
-        InlineKeyboardMarkup
-        )
+    Update,
+    InlineKeyboardButton,
+    InlineKeyboardMarkup
+)
 from telegram.ext import (
-        ContextTypes,
-        CallbackContext
-        )
+    ContextTypes,
+    CallbackContext, ConversationHandler
+)
 
 # Set up logging
 logger = logging.getLogger(__name__)
 
-ASKQRCODE, CONVERTURL = range(2)
+QRCODE_START, QRCODE_END = range(2)
+
 
 # Fonction qui sera appelée lors de la commande /start
 async def start(update: Update, context: CallbackContext) -> None:
-    """Sends a message with three inline buttons attached."""
+    logger.info("User %s started the conversation.", update.message.from_user.first_name)
     keyboard = [
         [
-            InlineKeyboardButton("Option 1", callback_data='1'),
-            InlineKeyboardButton("Option 2", callback_data='2'),
+            InlineKeyboardButton("Generate QR code", callback_data='qrcode')
         ],
-        [InlineKeyboardButton("Option 3", callback_data='3')],
+        [
+            InlineKeyboardButton("empty", callback_data='2')
+        ]
     ]
-
     reply_markup = InlineKeyboardMarkup(keyboard)
+    await update.message.reply_text('Choose command', reply_markup=reply_markup)
 
-    await update.message.reply_text('Please choose:', reply_markup=reply_markup) 
 
 # Fonction qui sera appelée lors de l'envoi d'un message texte
 async def echo(update: Update, context: ContextTypes.DEFAULT_TYPE):
@@ -42,15 +46,48 @@ async def unknown(update: Update, context: ContextTypes.DEFAULT_TYPE):
     await context.bot.send_message(chat_id=update.effective_chat.id, text="Sorry, I didn't understand that command.")
 
 
-async def askQrCode(update : Update, context : ContextTypes.DEFAULT_TYPE):
-    await context.bot.send_message(
-            chat_id=update.effective_chat.id,
-            text='Veuillez entrer un URL à convertir en QR code'
-            )
+async def ask_qrcode_url(update: Update, context: CallbackContext):
+    try:
+        await context.bot.send_message(
+                chat_id=update.effective_chat.id,
+                text='Veuillez entrer un URL à convertir en QR code'
+                )
+        return QRCODE_START
+    except Exception as e:
+        logger.exception("Error occurred whith the method ask_qrcode_url: %s", str(e))
+        await context.bot.send_message(chat_id=update.effective_chat.id, text="An error occurred whith your URL.")
 
 
-async def convertUrl(update : Update, context : ContextTypes.DEFAULT_TYPE):
-    url=pyqrcode.create(update.message.text)
-    url.png('qrcode.png',scale=15)
-    bot.send_chat_action(message.chat.id, 'upload_document')
-    bot.send_document(message.chat.id,open('qrcode.png','rb' ))
+async def get_qrcode(update: Update, context: CallbackContext):
+    try:
+        url = pyqrcode.create(update.message.text)
+        url.png('qrcode.png', scale=15)
+        await context.bot.send_chat_action(update.message.chat.id, 'upload_document')
+        await context.bot.send_document(update.message.chat.id, open('qrcode.png', 'rb'))
+        os.remove("qrcode.png")
+        return QRCODE_END
+    except Exception as e:
+        logger.exception("Error occurred while generating QR code: %s", str(e))
+        await context.bot.send_message(chat_id=update.effective_chat.id, text="An error occurred while generating the QR code.")
+
+
+# Definition de la fonction asynchrone de gestion du message contenant l'entrée texte de l'utilisateur
+async def error(update: Update, context: CallbackContext) -> None:
+    print(f'Update {update} cause error {context.error}')
+    logging.exception("ERROR")
+
+
+async def cancel(update, context) -> int:
+    context.user_data.clear()
+    context.bot.send_message(chat_id=update.effective_chat.id, text="Opération annulée.")
+    return ConversationHandler.END
+
+
+async def handle_callback_query(update: Update, context: CallbackContext):
+    query = update.callback_query
+    await query.answer() # Répondre au callback pour éviter une erreur de timeout
+    #await query.edit_message_text(text="Selected option: {}".format(query.data))
+    if query.data == "qrcode":
+        await ask_qrcode_url(update, context)
+    return ConversationHandler.END
+
